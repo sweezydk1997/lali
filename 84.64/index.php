1f14a80c8a7f39f7f5197289d94442e5e9ea806b
sweezydk1997 <153256015+sweezydk1997@users.noreply.github.com>
2023-12-20 21:30:12 -0500

    Add files via upload

---
+++ 84.64/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola Mundo en PHP</title>
+</head>
+<body>
+
+    <?php
+        echo "¡Hola Mundo!";
+    ?>
+
+</body>
+</html>
